164f2b528620cf162c9ee954b7ba19c6bd67a338
nukelet <nukelet@protonmail.com>
2023-06-23 14:20:25 -0300

    Update address for the TUN/TAP interface so as to avoid routing issues

--- run.sh
+++ run.sh
@@ -6,7 +6,7 @@ fi
 sudo setcap "cap_net_admin=eip" target/release/icmp-tuntap
 target/release/icmp-tuntap &
 pid=$!
-sudo ip addr add 192.168.0.1/24 dev tun0
+sudo ip addr add 10.0.0.0/24 dev tun0
 sudo ip link set up dev tun0
 trap "kill $pid" INT TERM
 wait $pid
